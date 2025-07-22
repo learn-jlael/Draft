5b028e016e7f9b75a6db25c90881a29ed7a6a959
Jonatas Lael <learn.jlael@gmail.com>
2025-07-22 18:43:45 -0300

    commit

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ==========================================
+// CONFIGURAÇÃO DO FIREBASE
+// ==========================================
 const firebaseConfig = {
-  apiKey: "AIzaSyDJHGhlZptKnE_um5l9KOjcZHcq6U0iIQE",
-  authDomain: "draft-sls.firebaseapp.com",
-  databaseURL: "https://draft-sls-default-rtdb.firebaseio.com",
-  projectId: "draft-sls",
-  storageBucket: "draft-sls.firebasestorage.app",
-  messagingSenderId: "721078818051",
-  appId: "1:721078818051:web:be9461a00a95b6f1e30536",
-  measurementId: "G-596730GMNJ"
+    apiKey: "AIzaSyDJHGhlZptKnE_um5l9KOjcZHcq6U0iIQE",
+    authDomain: "draft-sls.firebaseapp.com",
+    databaseURL: "https://draft-sls-default-rtdb.firebaseio.com",
+    projectId: "draft-sls",
+    storageBucket: "draft-sls.firebasestorage.app",
+    messagingSenderId: "721078818051",
+    appId: "1:721078818051:web:be9461a00a95b6f1e30536"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
